ca59784c6b23dbe36d2271fd1cded282e91305a9
Leli_n <122529284+LelionS@users.noreply.github.com>
2025-12-07 19:31:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+const { spawn } = require('child_process');
+const path = require('path');
 
 function start() {
-   let args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+    // Updated to match the renamed plugins file
+    let args = [path.join(__dirname, 'plugins.cjs'), ...process.argv.slice(2)];
+    console.log([process.argv[0], ...args].join('\n'));
+
+    let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    })
+    .on('message', data => {
+        if (data === 'reset') {
+            console.log('Restarting Bot...');
+            p.kill();
+            start();
+            delete p;
+        }
+    })
+    .on('exit', code => {
+        console.error('Exited with code:', code);
+        if (code === 0 || code === 1) start();
+    });
 }
-start()
+
+start();
